0f7c54431b2d7396bd9a0cfd19f3f60964616b75
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2022-12-26 09:41:03 +0500

    add Model

---
+++ lib/Pages/Model.dart
@@ -0,0 +1,15 @@
+class MenuModel {
+  final String name;
+  final int price;
+  final String description;
+  final String info;
+  final String image;
+
+  MenuModel({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.info,
+    required this.image,
+  });
+}
